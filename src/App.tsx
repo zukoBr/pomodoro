368066ccbb9e98d0d18fab203a7eae5d14d31811
André Luis Suckow <andre.suckow.dev@gmail.com>
2026-05-24 14:35:14 -0300

    teste mudança

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import './App.css'
+import './App.css';
 
 function App() {
-
   return (
     <>
-     <h1 className="center">Hello, Vite + React!</h1>
+      <h1 className='center'>Hello, Vite + React!!!</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
